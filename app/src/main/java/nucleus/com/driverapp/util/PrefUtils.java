989ff9adb9f29786744538d158d0b1025cc9c47b
kevintan <kevintanhongann@gmail.com>
2018-01-14 22:42:49 +0800

    more stubs

--- app/src/main/java/nucleus/com/driverapp/util/PrefUtils.java
+++ app/src/main/java/nucleus/com/driverapp/util/PrefUtils.java
@@ -1,9 +1,18 @@
 package nucleus.com.driverapp.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by kevintan on 10/11/2017.
  */
-
 public class PrefUtils {
+
+
+    public static SharedPreferences getPreferences(Context context) {
+        //return PreferenceManager.getDefaultSharedPreferences(context);
+        return context.getSharedPreferences("nucleus.com.driverapp", Context.MODE_PRIVATE);
+    }
+
 
 }
