b3d1a53c73f9a5d7b5d1cb2cf774d9920cc4c438
kevintan <kevintanhongann@gmail.com>
2018-01-28 13:00:31 +0800

    prefutils

--- app/src/main/java/nucleus/com/driverapp/util/PrefUtils.java
+++ app/src/main/java/nucleus/com/driverapp/util/PrefUtils.java
@@ -8,11 +8,27 @@ import android.content.SharedPreferences;
  */
 public class PrefUtils {
 
+    private static final String REFRESHTOKEN_PREFS = "nucleus.com.driverapp.refreshtoken";
+    private static final String AUTHTOKEN_PREFS = "nucleus.com.driverapp.authtoken";
 
     public static SharedPreferences getPreferences(Context context) {
         //return PreferenceManager.getDefaultSharedPreferences(context);
         return context.getSharedPreferences("nucleus.com.driverapp", Context.MODE_PRIVATE);
     }
 
+    public static void setRefreshToken(String refreshToken, Context context){
+        getPreferences(context).edit().putString(REFRESHTOKEN_PREFS, refreshToken).apply();
+    }
 
+    public static void setAuthToken(String authToken, Context context){
+        getPreferences(context).edit().putString(AUTHTOKEN_PREFS, authToken).apply();
+    }
+
+    public static String getRefreshToken(Context context){
+        return getPreferences(context).getString(REFRESHTOKEN_PREFS, "");
+    }
+
+    public static String getAuthToken(Context context){
+        return getPreferences(context).getString(AUTHTOKEN_PREFS, "");
+    }
 }
