33bb692b164e214e2efeea5ac9c5bb39fb44d21f
kevintan <kevintanhongann@gmail.com>
2018-04-01 11:58:50 +0800

    wip

--- app/src/main/java/nucleus/com/driverapp/forgetpassword/ForgetPasswordActivity.java
+++ app/src/main/java/nucleus/com/driverapp/forgetpassword/ForgetPasswordActivity.java
@@ -6,8 +6,9 @@ import android.os.Bundle;
 import butterknife.OnClick;
 import nucleus.com.driverapp.DriverApplication;
 import nucleus.com.driverapp.R;
+import nucleus.com.driverapp.response.ForgetPasswordResponse;
 
-public class ForgetPasswordActivity extends AppCompatActivity {
+public class ForgetPasswordActivity extends AppCompatActivity implements ForgetPasswordView{
 
 
 
@@ -22,6 +23,26 @@ public class ForgetPasswordActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_request_reset)
     void requestReset(){
+
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void onSuccess(ForgetPasswordResponse response) {
+
+    }
+
+    @Override
+    public void onFailure(Exception ex) {
 
     }
 }
---
+++ app/src/main/java/nucleus/com/driverapp/phonelogin/PhoneLoginActivity.java
@@ -0,0 +1,24 @@
+package nucleus.com.driverapp.phonelogin;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import butterknife.OnClick;
+import nucleus.com.driverapp.BaseActivity;
+import nucleus.com.driverapp.R;
+
+public class PhoneLoginActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phonelogin);
+    }
+
+
+    void phoneLoginClick(){
+
+    }
+
+
+}
